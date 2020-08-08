fb85c4169136a9d5eeec856bbe8bddf15047cd76
ishaan09kapoor <ishaan09kapoor@gmail.com>
2020-08-08 17:31:18 +0530

    Add files via upload

---
+++ Jio/Jio/jio/src/main/java/com/msp/jio/modelassembler/JioModelAssembler.java
@@ -0,0 +1,24 @@
+package com.msp.jio.modelassembler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+
+import com.msp.jio.controller.JioController;
+import com.msp.jio.entity.Jio;
+
+/**
+ * This is a model assembler class and is user to make our work easy
+ * @author ishaankapoor
+ *
+ */
+@Component
+public class JioModelAssembler implements RepresentationModelAssembler<Jio, EntityModel<Jio>>{
+	public EntityModel<Jio> toModel(Jio jio){
+		return EntityModel.of(jio, //
+			      linkTo(methodOn(JioController.class).viewBalance(jio.getMobileNumber())).withSelfRel(), //
+			      linkTo(methodOn(JioController.class).allNumbers()).withRel("viewBalance"));
+	}
+}
